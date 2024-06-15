a0afeba37ccc0eb903ac74010674e5e6e658a57c
Ryan O'Leary <ryanaoleary@google.com>
2024-06-15 10:55:27 +0000

    Add tpu example for Ray serve with stable diffusion
    Remove serve run line
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>
    
    Add _generate positional args
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>
    
    Revert back
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>
    
    Remove erroring await statement
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>
    
    Revert change
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>
    
    changed await call
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>
    
    Change api ingress
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>
    
    Fix init
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>
    
    Fix generate call
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>
    
    Fix generate call
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>
    
    Move and rename stable diffusion tpu
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

---
+++ stable_diffusion/stable_diffusion_tpu.py
@@ -0,0 +1,140 @@
+"""Ray Serve Stable Diffusion example."""
+from io import BytesIO  # pylint:disable=g-importing-member
+from typing import List
+from fastapi import FastAPI  # pylint:disable=g-importing-member
+from fastapi.responses import Response  # pylint:disable=g-importing-member
+import ray
+from ray import serve
+
+app = FastAPI()
+_MAX_BATCH_SIZE = 64
+
+@serve.deployment(num_replicas=1)
+@serve.ingress(app)
+class APIIngress:
+    def __init__(self, diffusion_model_handle) -> None:
+        self.handle = diffusion_model_handle
+
+    @app.get(
+        "/imagine",
+        responses={200: {"content": {"image/png": {}}}},
+        response_class=Response,
+    )
+    async def generate(self, prompt: str):
+        assert len(prompt), "prompt parameter cannot be empty"
+
+        image = await self.handle.generate.remote(prompt)
+        return image
+
+
+@serve.deployment(
+    ray_actor_options={
+        "resources": {"TPU": 4},
+    })
+class StableDiffusion:
+  """FLAX Stable Diffusion Ray Serve deployment running on TPUs.
+
+  Attributes:
+    run_with_profiler: Whether or not to run with the profiler. Note that
+      this saves the profile to the separate TPU VM.
+
+  """
+
+  def __init__(
+      self, run_with_profiler: bool = False, warmup: bool = False,
+      warmup_batch_size: int = _MAX_BATCH_SIZE):
+    from diffusers import FlaxStableDiffusionPipeline  # pylint:disable=g-import-not-at-top,g-importing-member
+    from flax.jax_utils import replicate  # pylint:disable=g-import-not-at-top,g-importing-member
+    import jax  # pylint:disable=g-import-not-at-top,unused-import
+    import jax.numpy as jnp  # pylint:disable=g-import-not-at-top
+    from jax import pmap  # pylint:disable=g-import-not-at-top,g-importing-member
+
+    model_id = "CompVis/stable-diffusion-v1-4"
+
+    self._pipeline, params = FlaxStableDiffusionPipeline.from_pretrained(
+        model_id,
+        revision="bf16",
+        dtype=jnp.bfloat16)
+
+    self._p_params = replicate(params)
+    self._p_generate = pmap(self._pipeline._generate)
+    self._run_with_profiler = run_with_profiler
+    self._profiler_dir = "/tmp/tensorboard"
+
+    if warmup:
+      print("Sending warmup requests.")
+      warmup_prompts = ["A warmup request"] * warmup_batch_size
+      self.generate_tpu(warmup_prompts)
+
+  def generate_tpu(self, prompts: List[str]):
+    """Generates a batch of images from Diffusion from a list of prompts.
+
+    Args:
+      prompts: a list of strings. Should be a factor of 4.
+
+    Returns:
+      A list of PIL Images.
+    """
+    from flax.training.common_utils import shard  # pylint:disable=g-import-not-at-top,g-importing-member
+    import jax   # pylint:disable=g-import-not-at-top
+    import time  # pylint:disable=g-import-not-at-top
+    import numpy as np  # pylint:disable=g-import-not-at-top
+
+    rng = jax.random.PRNGKey(0)
+    rng = jax.random.split(rng, jax.device_count())
+
+    assert prompts, "prompt parameter cannot be empty"
+    print("Prompts: ", prompts)
+    prompt_ids = self._pipeline.prepare_inputs(prompts)
+    prompt_ids = shard(prompt_ids)
+    print("Sharded prompt ids has shape:", prompt_ids.shape)
+    if self._run_with_profiler:
+      jax.profiler.start_trace(self._profiler_dir)
+
+    time_start = time.time()
+    images = self._p_generate(prompt_ids, self._p_params, rng)
+    images = images.block_until_ready()
+    elapsed = time.time() - time_start
+    if self._run_with_profiler:
+      jax.profiler.stop_trace()
+
+    print("Inference time (in seconds): ", elapsed)
+    print("Shape of the predictions: ", images.shape)
+    images = images.reshape(
+        (images.shape[0] * images.shape[1],) + images.shape[-3:])
+    print("Shape of images afterwards: ", images.shape)
+    return self._pipeline.numpy_to_pil(np.array(images))
+
+  @serve.batch(batch_wait_timeout_s=10, max_batch_size=_MAX_BATCH_SIZE)
+  async def batched_generate_handler(self, prompts: List[str]):
+    """Sends a batch of prompts to the TPU model server.
+
+    This takes advantage of @serve.batch, Ray Serve's built-in batching
+    mechanism.
+
+    Args:
+      prompts: A list of input prompts
+
+    Returns:
+      A list of responses which contents are raw PNG.
+    """
+    print("Number of input prompts: ", len(prompts))
+    num_to_pad = _MAX_BATCH_SIZE - len(prompts)
+    prompts += ["Scratch request"] * num_to_pad
+
+    images = self.generate_tpu(prompts)
+    results = []
+    for image in images[: _MAX_BATCH_SIZE - num_to_pad]:
+      file_stream = BytesIO()
+      image.save(file_stream, "PNG")
+      results.append(
+          Response(content=file_stream.getvalue(), media_type="image/png")
+      )
+    return results
+
+  async def generate(self, prompt):
+    return await self.batched_generate_handler(prompt)
+
+
+diffusion_bound = StableDiffusion.bind()
+deployment = APIIngress.bind(diffusion_bound)
